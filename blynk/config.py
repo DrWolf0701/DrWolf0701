90cd60ed27d305552d80a1551a1bb86b41f4d19d
DrWolf0701 <s8824415@gmail.com>
2024-09-29 11:52:37 +0800

    modify

--- blynk/config.py
+++ blynk/config.py
@@ -1,7 +1,7 @@
 # 在此填入 Blynk Device Info 的資訊
 BLYNK_TEMPLATE_ID   = ""
 BLYNK_TEMPLATE_NAME = "MQTT Demo"
-BLYNK_AUTH_TOKEN    = ""
+BLYNK_AUTH_TOKEN    = "9u_cyxJDFis72V3LmqulO9Upr3IMT1h-"
 
 # 變更預設 Blynk 伺服器。
 BLYNK_MQTT_BROKER   = "blynk.cloud"
